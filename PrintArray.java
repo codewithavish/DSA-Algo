9f7dd4256bc3cfd41b65485a6bf0ed1403b95776
codewithavish <writeonaviral@gmail.com>
2025-02-04 00:27:17 +0530

    commit

---
+++ PrintArray.java
@@ -0,0 +1,15 @@
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 21;
+        arr[1] = 31;
+        arr[2] = 41;
+        arr[3] = 51;
+        arr[4] = 61;
+
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
--- Sum.java
+++ Sum.java
@@ -2,6 +2,7 @@ public class Sum{
     public static void main(String[] args) {
         int a =10;
         int b = 20;
-        System.out.println("The Sum is" + a+b);
+        int sum  = a + b;
+        System.out.println("The Sum is " + sum);
     }
 }
